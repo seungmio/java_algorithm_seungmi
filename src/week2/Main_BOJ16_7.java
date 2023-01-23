30a3b5fbdc8d97537b2b9cbd844f30d5b07af9a9
seungmio <68382824+seungmio@users.noreply.github.com>
2023-01-24 03:43:21 +0900

    [16 단계] 백트래킹 7, 8번 (8번 미해결)

---
+++ src/week2/Main_BOJ16_7.java
@@ -0,0 +1,78 @@
+package week2;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main_BOJ16_7 {
+    public static int n;    //수의 개수
+    public static long[] arr;    //수를 담을 배열
+    public static int[] operator = new int[4];   //연산자 수를 담을 배열
+    //static int MAX = Integer.MAX_VALUE;    //최댓값
+    //static int MIN = Integer.MIN_VALUE;    //최솟값
+
+    static long MAX = -1100000000;
+    static long MIN = 1100000000;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        arr = new long[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        //Exception in thread "main" java.util.NoSuchElementException 오류
+        // 더이상 element가 없는데도 불러오려고 할 때 발생
+        st = new StringTokenizer(br.readLine());
+        //+ - x ÷ 순
+        for (int i = 0; i < 4; i++) {
+            operator[i] = Integer.parseInt(st.nextToken());
+        }
+
+        recursion(arr[0],1);
+
+        //출력 테스트용
+        //숫자 2147483647
+        //-2147483648 이지랄남 왜이래 -> long 으로 초기화 해주니까 해결됨
+        sb.append(MAX + "\n");
+        sb.append(MIN);
+
+        bw.write(String.valueOf(sb));
+        bw.flush();
+        bw.close();
+    }
+
+    //연산자 배열을 반복하면서 최대, 최솟값을 갱신해야 함
+    public static void recursion(long num, int cnt) {
+        //cnt 값이 n과 같아지면 모든 수가 계산된 것이므로 max, min 값을 갱신한다.
+        if (cnt == n) {
+            //Math 함수 -> 두 인자를 비교하여 최대, 최소값을 return
+            MAX = Math.max(MAX, num);
+            MIN = Math.min(MIN, num);
+            return;
+        }
+
+        for (int i = 0; i < 4; i++) {
+            if (operator[i] > 0) {  //연산자 개수가 1개 이상이어야 함.
+                operator[i]--;  //해당 연산자 사용
+
+                if (i == 0) {   //+
+                    recursion(num + arr[cnt], cnt + 1);
+                } else if (i == 1) { //-
+                    recursion(num - arr[cnt], cnt + 1);
+                } else if (i == 2) {    //*
+                    recursion(num * arr[cnt], cnt + 1);
+                } else if (i == 3) { // /
+                    recursion(num / arr[cnt], cnt + 1);
+                }
+
+                operator[i]++;
+            }
+        }
+    }
+}
---
+++ src/week2/Main_BOJ16_8.java
@@ -0,0 +1,77 @@
+package week2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_BOJ16_8 {
+    /*
+    12 / 34 -> (1,2 + 2,1), (3,4 + 4,3)
+    가능한 모든 스타트 팀과 링크 팀의 능력치를 구해서 그 차이의 최솟값을 계산
+     */
+    static int n;   //n 명
+    static int[][] arr;   //n * n 배열
+    static boolean[] isCheck;   //1 ~ n 까지 방문 유무 검토
+    static int MIN = Integer.MAX_VALUE;   //두 팀의 능력치 최솟값
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        isCheck = new boolean[n];
+
+        arr = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        recursion(0, 0);
+        System.out.println(MIN);
+    }
+
+    public static void recursion(int num, int cnt) {
+        if (cnt == n / 2) { //두 팀으로 나누어 능력치 합을 구한다. (무조건 짝수)
+            /*
+            조건문 안에 전부 구현하지 말고 따로 함수로 정리하자
+            여기서도 long 문제 생겨서 검색함.
+             */
+            diff(); //능력치 차이를 구하는 함수
+            return;
+        }
+
+        for (int i = num; i < n; i++) {
+            if (!isCheck[i]) {  //방문하지 않은 사람이라면
+                isCheck[i] = true;  //방문한 사람으로 변경
+                recursion(num + 1,cnt + 1);
+                isCheck[i] = false;
+            }
+        }
+    }
+
+    public static void diff() {
+        int start = 0;  //스타트 팀 능력치
+        int link = 0;   //링크 팀 능력치
+
+        // [0, 0] .. [3, 4] 를 방지하기 위해 i, j 초기화 (1번만 탐색)
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (isCheck[i] && isCheck[j]) { //i, j 가 모두 true 면,
+                    //스타트 팀으로 합계
+                    start += arr[i][j];
+                    start += arr[j][i];
+                } else if (!(isCheck[i] && isCheck[j])) {   //i, j 가 모두 false 면,
+                    //링크 팀으로 합계
+                    link += arr[i][j];
+                    link += arr[j][i];
+                }
+            }
+        }
+
+        //차이
+        int diff = Math.abs(start - link);
+        MIN = Math.min(diff, MIN);
+    }
+}
